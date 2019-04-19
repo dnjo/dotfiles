7d93a460aeb501a28c4ff1e722c03bd7c73ec9af
Dennis Johansson <dennis@dnjo.net>
2019-04-19 16:27:52 +0200

    Check for command presence

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 [ -f $DOTFILES/aliases_local.sh ] && source $DOTFILES/aliases_local.sh
 
-alias git=hub
+command -v hub >/dev/null 2>&1 && alias git=hub
 alias gdh="git diff HEAD^ HEAD"
 alias v=nvim
--- zsh_profile.sh
+++ zsh_profile.sh
@@ -2,24 +2,29 @@ source $DOTFILES/env.sh
 source $DOTFILES/functions.sh
 source $DOTFILES/aliases.sh
 
-export PATH="$HOME/.exenv/bin:$PATH"
-eval "$(exenv init -)"
-eval "$(rbenv init -)"
+if command -v >/dev/null 2>&1; then
+    export PATH="$HOME/.exenv/bin:$PATH"
+    eval "$(exenv init -)"
+    eval "$(rbenv init -)"
+fi
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-. /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+powerline_path=/usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+[ -f $powerline_path ] && . $powerline_path
+#. /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 USER_BASE_PATH=$(python -m site --user-base)
 export PATH=$PATH:$USER_BASE_PATH/bin
 
-export PATH=$PATH:/usr/local/MacGPG2/bin
-export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
-if ! pgrep -x "gpg-agent" > /dev/null
-then
-    gpg-agent --daemon --enable-ssh-support > /dev/null
+if [ -d /usr/local/MacGPG2 ]; then
+    export PATH=$PATH:/usr/local/MacGPG2/bin
+    export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
+    if ! pgrep -x "gpg-agent" > /dev/null; then
+        gpg-agent --daemon --enable-ssh-support > /dev/null
+    fi
 fi
