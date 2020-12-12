d04a093baea56e8051a17498361461272a33a4d8
Dennis Johansson <dennis@dnjo.net>
2020-12-12 22:03:29 +0100

    Fix indentation

--- zsh_profile.sh
+++ zsh_profile.sh
@@ -21,7 +21,7 @@ if [ -n "$DOTFILES_GPG_AGENT" ]; then
     export GPG_TTY="$(tty)"
     export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
     if ! pgrep -x "gpg-agent" > /dev/null; then
-	gpgconf --launch gpg-agent
+	    gpgconf --launch gpg-agent
     fi
 fi
 
