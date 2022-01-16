27d2e2f3cc6cef4f4d5e9e4294cae801eba1e50c
Dennis Johansson <dennis@dnjo.net>
2022-01-16 14:58:39 +0100

    Clean up shell configuration
    - Move files to shell directory
    - Remove unused stuff

--- aliases.sh
+++
@@ -1,9 +0,0 @@
-[ -f $DOTFILES/aliases_local.sh ] && source $DOTFILES/aliases_local.sh
-
-alias gdh="git diff HEAD^ HEAD"
-alias glu="glo @{u}...HEAD"
-alias gst='nvim -c "Gstatus"'
-alias v=nvim
-alias r=ranger
-alias q=escape-ranger
-alias refresh-yubikey='gpg-connect-agent "scd serialno" "learn --force" /bye > /dev/null'
