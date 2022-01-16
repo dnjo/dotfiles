76c7b962946b37c784f3fb3a0676755778c8cfaf
Dennis Johansson <dennis@dnjo.net>
2022-01-16 15:00:45 +0100

    WIP

---
+++ neovim/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+ln -s "$(pwd)/vimrc" ~/.vimrc
+mkdir -p ~/.config/nvim
+ln -s "$(pwd)/init.vim" ~/.config/nvim/init.vim
+ln -s "$(pwd)/colors" ~/.config/nvim/colors
+
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | /bin/bash -s - ~/.cache/dein
